34194ce10d26677b6f5b2cb12af7a0cf5ec08602
manognabandaru <manogna.bandaru@gmail.com>
2018-07-24 20:03:27 -0500

    Two Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  itemCount: number;
+  userid = 'id'; 
+  name = 'name';
+  technology = 'technology';
+  useridlist =[];
+  namelist =[];
+  technologylist =[];
+  custom;
+  constructor(){}
+  ngOnInit(){
+    // this.itemCount=this.list.length;
+  }
+  // onChange(reference){
+  //  this.useridlist.push(this.userid);
+  //  this.namelist.push(this.name);
+  //  this.technologylist.push(this.technology);
+  //  this.userid='';
+  //  this.name='';
+  //  this.technology='';
+  //  this.itemCount=this.list.length;
+
+  // }
+  onChange(val1, val2, val3): any{
+    this.useridlist.push(val1);
+    this.namelist.push(val2);
+    this.technologylist.push(val3);
+    this.userid='';
+    this.name='';
+    this.technology='';
+    // this.itemCount=this.list.length;
+ 
+   }
+  //<!-- for template reference eg-->
+  // customMethod(value: any) {
+  //   this.custom = value;
+  // }
 }
